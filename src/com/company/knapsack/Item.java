3ae87fce6c53f281371055167855a8517cf3bc41
Yousef <Yousef.a.altaher@gmail.com>
2022-02-05 13:02:49 +0000

    Knapsack problem
    This commit is intended to provide a GA solution to the Knapsack problem.

---
+++ src/com/company/knapsack/Item.java
@@ -0,0 +1,36 @@
+package com.company.knapsack;
+
+/**
+ * This class is intended to represent an item in a knapsack
+ */
+public class Item {
+
+    private double price;
+    private double weight;
+
+    /**
+     *
+     * @param price item's price
+     * @param weight item's weight
+     */
+    public Item(double price, double weight){
+        this.price = price;
+        this.weight = weight;
+    }
+
+    /**
+     *
+     * @return item's price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     *
+     * @return item's weight
+     */
+    public double getWeight() {
+        return weight;
+    }
+}
